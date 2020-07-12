44f708b5129b900d842a16e3ed5247f8e73d369f
hk7math <hk7math@gmail.com>
2020-07-12 14:08:56 +0800

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,26 @@ const totalLikes = (blogs) => {
   return blogs.reduce((prev, blog) => prev + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? {}
+    : blogs.reduce(
+      (prev, blog) => blog.likes > prev.likes
+        ? {
+          title: blog.title, 
+          author: blog.author, 
+          likes: blog.likes
+        }
+        : prev ,
+      {
+        title: blogs[0].title, 
+        author: blogs[0].author, 
+        likes: blogs[0].likes
+      })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
